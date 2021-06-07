bc6ca084b07a25e4370ff55cd2d3dc8f385d2f9c
Rison <2462301753@qq.com>
2021-06-07 11:50:40 +0800

    feat 分流split/select、合流connect/comap、联合union

--- src/main/scala/com/rison/apitest/TransformTest.scala
+++ src/main/scala/com/rison/apitest/TransformTest.scala
@@ -9,8 +9,7 @@ import org.apache.flink.streaming.api.scala._
 object TransformTest {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(1)
-    val stream: DataStream[String] = env.readTextFile("data/sensor.txt").setParallelism(1)
+    val stream: DataStream[String] = env.readTextFile("data/sensor.txt")
     //转换类型
     val streamMap: DataStream[SensorReading] = stream.map(
       data => {
@@ -28,7 +27,32 @@ object TransformTest {
         SensorReading(curState.id, newData.timestamp, curState.temperature.min(newData.temperature))
       }
     )
-    reduceMap.print()
+//    reduceMap.print()
+
+    //分流操作 split
+    val splitStream: SplitStream[SensorReading] = streamMap.split(
+      data => {
+        if (data.temperature >= 35.6) Seq("high") else Seq("low")
+      }
+    )
+    val highStream: DataStream[SensorReading] = splitStream.select("high")
+    val lowStream: DataStream[SensorReading] = splitStream.select("low")
+//    highStream.print("high")
+//    lowStream.print("low")
+
+    //合流操作 connect 数据类型可以不一致
+    val warningStream: DataStream[(String, Double)] = highStream.map(data => (data.id, data.temperature))
+    val connectStreams: ConnectedStreams[(String, Double), SensorReading] = warningStream.connect(lowStream)
+    // 用coMap对数据分别处理
+    val coMapResultStream: DataStream[Product] = connectStreams.map(
+      warningData => (warningData._1, warningData._2, "warning"),
+      lowData => (lowData.id, "healthy")
+    )
+//    coMapResultStream.print()
+    //联合 数据类型一致
+    val unionStream: DataStream[SensorReading] = highStream.union(lowStream)
+    unionStream.print()
+
     env.execute("transform test")
   }
 
