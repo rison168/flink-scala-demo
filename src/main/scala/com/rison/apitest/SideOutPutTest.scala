fb6273ddaf7279cefe9f52c5832c33e49be375e3
Rison <2462301753@qq.com>
2021-06-09 15:41:25 +0800

    feat processFunction 侧输出流实现

---
+++ src/main/scala/com/rison/apitest/SideOutPutTest.scala
@@ -0,0 +1,41 @@
+package com.rison.apitest
+
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+
+/**
+ * @author : Rison 2021/6/9 下午3:24
+ *
+ */
+object SideOutPutTest {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    //从文件读取数据
+    val stream: DataStream[String] = env.readTextFile("data/sensor.txt")
+    //转换类型
+    val streamMap: DataStream[SensorReading] = stream.map(
+      data => {
+        val arr = data.split(" ")
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+      }
+    )
+    val highTempStream: DataStream[SensorReading] = streamMap.process(SplitTempProcessor(30.0))
+    highTempStream.print("high")
+    highTempStream.getSideOutput(new OutputTag[(String, Long, Double)]("low")).print("low")
+    env.execute("SideOutPutTest" )
+  }
+}
+
+case class SplitTempProcessor(temperature: Double) extends ProcessFunction[SensorReading, SensorReading]{
+  override def processElement(i: SensorReading, context: ProcessFunction[SensorReading, SensorReading]#Context, collector: Collector[SensorReading]): Unit = {
+    if (i.temperature > temperature){
+      //如果当前温度大于30，输出到主流
+      collector.collect(i)
+    }else{
+      //不超过30，输出到侧输出流
+      context.output(new OutputTag[(String, Long, Double)]("late"), (i.id, i.timestamp, i.temperature))
+    }
+  }
+}
